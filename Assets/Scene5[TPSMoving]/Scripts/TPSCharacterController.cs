ab4f66c83c1116dc040a3eb902e3779e195ed29a
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-01-11 00:12:48 +0900

    Joystick Update

--- Assets/Scene5[TPSMoving]/Scripts/TPSCharacterController.cs
+++ Assets/Scene5[TPSMoving]/Scripts/TPSCharacterController.cs
@@ -18,12 +18,14 @@ public class TPSCharacterController : MonoBehaviour
 
     private void Update()
     {
-        LookAround();
-        Move();
+        //LookAround();
+        //Move();
     }
 
+    //키보드 방향키 이동
     private void Move()
     {
+        
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         bool isMove = moveInput.magnitude != 0;
         animator.SetBool("isMove", isMove);
@@ -39,6 +41,26 @@ public class TPSCharacterController : MonoBehaviour
         }
     }
 
+    //조이스틱 이동
+    public void MoveJoystick(Vector2 inputDirection)
+    {
+
+        Vector2 moveInput = inputDirection;
+        bool isMove = moveInput.magnitude != 0;
+        animator.SetBool("isMove", isMove);
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = lookForward;//FPS식
+            //characterBody.forward = moveDir;//RPG식
+            transform.position += moveDir * Time.deltaTime * 5f;//이동속도
+        }
+    }
+
+    //마우스 방향 회전
     private void LookAround()
     {
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
@@ -49,6 +71,26 @@ public class TPSCharacterController : MonoBehaviour
         //한국식: camAngle.x - mouseDelta.y
         //미국식: camAngle.x + mouseDelta.y
         if(x < 180f)
+        {
+            x = Mathf.Clamp(x, -1f, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 335f, 361f);
+        }
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+    //조이스틱 방향 회전
+    public void LookAroundJoystick(Vector3 inputDirection)
+    {
+        Vector2 mouseDelta = inputDirection;
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+
+
+        float x = camAngle.x - mouseDelta.y;
+        //한국식: camAngle.x - mouseDelta.y
+        //미국식: camAngle.x + mouseDelta.y
+        if (x < 180f)
         {
             x = Mathf.Clamp(x, -1f, 70f);
         }
---
+++ Assets/Scene6[Joystick]/Scripts/VirtualJoystick.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;//키보드, 마우스, 터치를 이벤트로 오브젝트에 보낼 수 있는 기능 지원
+
+public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{   
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
+
+    [SerializeField, Range(10, 150)]
+    private float leverRange;
+
+    private Vector2 inputDirection;
+    private bool isInput;
+
+    [SerializeField]
+    private TPSCharacterController controller;
+
+    public enum JoystickType { Move, Rotate, Count };
+    public JoystickType joystickType;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoustickLever(eventData);
+        isInput = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoustickLever(eventData);
+    }
+
+    private void ControlJoustickLever(PointerEventData eventData)
+    {
+        //터치 위치: eventData.position
+        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;//inputVector는 해상도를 기반으로 만들어져, 이를 기반으로 사용하면 너무 큰 값임
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+        switch (joystickType)
+        {
+            case JoystickType.Move:
+                controller.MoveJoystick(Vector2.zero);
+                break;
+            case JoystickType.Rotate:
+                break;
+        }
+    }
+
+    private void InputControlVector()
+    {
+        //캐릭터에게 입력벡터를 전달
+        Debug.Log(inputDirection.x + " / " + inputDirection.y);
+        switch (joystickType)
+        {
+            case JoystickType.Move:
+                controller.MoveJoystick(inputDirection);
+                break;
+            case JoystickType.Rotate:
+                controller.LookAroundJoystick(inputDirection);
+                break;
+        }   
+    }
+
+    private void Update()
+    {
+        if (isInput)
+        {
+            InputControlVector();
+        }
+    }
+}
