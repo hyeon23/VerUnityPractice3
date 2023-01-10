0646dd70a6537efbcffb4b464303f96da807e710
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-01-10 15:58:26 +0900

    3D Mouse Moving Update

--- Assets/Scene3[ObjectPooling]/Scripts/ObjectPool.cs
+++ Assets/Scene3[ObjectPooling]/Scripts/ObjectPool.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class ObjectPool : MonoBehaviour
 {
     public static ObjectPool Instance;
--- Assets/Scene4[3DMouseMoving]/Scripts/ClickMovement.cs
+++ Assets/Scene4[3DMouseMoving]/Scripts/ClickMovement.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class ClickMovement : MonoBehaviour
 {
     private Camera camera;
     private Animator animator;
+    private NavMeshAgent agent;
 
     private bool isMove;
     private Vector3 destination;
@@ -13,7 +14,10 @@ public class ClickMovement : MonoBehaviour
     private void Awake()
     {
         camera = Camera.main;
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        //agent.updatePosition = false: 이동 금시
+        agent.updateRotation = false;//회전 금지
     }
 
     private void Update()
@@ -27,11 +31,13 @@ public class ClickMovement : MonoBehaviour
             }
         }
 
-        Move();
+        LookMoveDirection();
     }
 
     private void SetDestination(Vector3 dest)
     {
+        //NavMeshAgent.SetDestination()으로 이동
+        agent.SetDestination(dest);
         destination = dest;
         isMove = true;
         animator.SetBool("isMove", true);
@@ -42,7 +48,7 @@ public class ClickMovement : MonoBehaviour
         if (isMove)
         {
             var dir = destination - transform.position;
-            transform.forward = dir;
+            animator.transform.forward = dir;
             transform.position += dir.normalized * Time.deltaTime * 5f;
         }
 
@@ -50,6 +56,23 @@ public class ClickMovement : MonoBehaviour
         {
             isMove = false;
             animator.SetBool("isMove", false);
+        }
+    }
+
+    public void LookMoveDirection()
+    {
+        if (isMove)
+        {
+            if (agent.velocity.magnitude == 0f)
+            {
+                isMove = false;
+                animator.SetBool("isMove", false);
+                return;
+            }
+
+            //높이가 다른 목적지를 클릭하면 회전하는 현상을 막기 위한 방식
+            var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position;
+            animator.transform.forward = dir;
         }
     }
 }
